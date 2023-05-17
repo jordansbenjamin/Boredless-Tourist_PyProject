b5d22e2116a985a98760781f92d8bfc83c955822
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 21:19:54 +1000

    Added function to generate message for traveler and present attractions they might be interested in

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_traveler_location(traveler):
     return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+# print(test_destination_index)
 
 # Using list comprehension to create an empty list of lists, with a specified n of empty lists using range()
 attractions = [[] for _ in range(len(destinations))]
@@ -64,4 +64,16 @@ def find_attractions(destination, interests):
     return attractions_with_interest
 
 la_arts = find_attractions("Los Angeles, USA", ['art'])
-print(la_arts)
+# print(la_arts)
+
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+    interests_string = f"Hi {traveler[0]}, we think you'll like these places around {traveler_destination}: "
+    for attraction in traveler_attractions:
+        interests_string += attraction
+    return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
