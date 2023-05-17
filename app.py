33ec3f8584d61de0ad3a3e753de18a85184c4d38
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 21:10:09 +1000

    Added interest finder logic

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ attractions = [[] for _ in range(len(destinations))]
 # for _ in range(len(destinations)):
 #     attractions.append([])
 
-print(attractions)
+# print(attractions)
 
 def add_attraction(destination, attraction):
     destination_index = get_destination_index(destination)
@@ -49,4 +49,19 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
+# print(attractions)
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
